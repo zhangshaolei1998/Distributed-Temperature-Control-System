8fbc5ef1591d2af42704b35a16695fc83cc8e9b6
learningordeath <sdwfyfybh@163.com>
2019-05-22 18:04:01 +0800

    service

--- class/Service.py
+++ class/Service.py
@@ -1,1 +1,20 @@
+import time
+
+
 class Service:
+
+    def __init__(self):
+        self.start_time = time.localtime(time.time())
+        self.end_time = time.localtime(time.time())
+        self.temperature = 26
+        self.fan_speed = 0
+
+    def set_temperature(self, temp):
+        self.temperature = temp
+
+    def set_fan_speed(self, speed):
+        self.fan_speed = speed
+
+    def get_info(self):
+        info = [self.start_time, self.end_time, self.temperature, self.fan_speed]
+        return info
