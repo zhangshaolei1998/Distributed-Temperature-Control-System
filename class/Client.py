063d0a65c0a7f9fea9ea418ed0204a614b41ab89
martyLY <741360647@qq.com>
2019-05-22 16:47:15 +0800

    Commit Class

---
+++ class/Client.py
@@ -0,0 +1,24 @@
+import Dispatcher
+
+class Client:
+
+    def __init__(self, room_id, current_room_temp, target_temp, fan_speed):
+        self.RoomId = room_id
+        self.current_room_temp = current_room_temp
+        self.target_temp = target_temp
+        self.fan_speed = fan_speed
+
+
+    def request_on(self):
+
+
+    def request_off(self):
+
+
+    def change_target_temp(self):
+
+
+    def change_fan_speed(self):
+
+
+    def request_fee(self):
