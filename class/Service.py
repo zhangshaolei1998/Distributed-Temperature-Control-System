31f303c3d45443859cd3baa9f4de2b3ea2d2c9c7
learningordeath <sdwfyfybh@163.com>
2019-05-23 11:43:56 +0800

    service

--- class/Service.py
+++ class/Service.py
@@ -6,12 +6,22 @@ class Service:
 
     # 初始化对象，传入室温 indoor_temp
     def __init__(self, indoor_temp):
-        self.start_time = time.localtime(time.time())
-        self.current_time = time.localtime(time.time())
-        self.end_time = time.localtime(time.time())
+        self.start_time = time.time()
+        self.current_time = time.time()
+        self.end_time = time.time()
+        self.service_time = 0       # 服务时长
+        self.wait_time = 0          # 等待时长
         self.indoor_temp = indoor_temp
         self.temperature = 26
         self.fan_speed = 0
+
+    # 更新服务时长
+    def set_service_time(self, service_time):
+        self.service_time = service_time
+
+    # 更新等待时长
+    def set_wait_time(self, wait_time):
+        self.wait_time = wait_time
 
     # 设置温度
     def set_temperature(self, temp):
