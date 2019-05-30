5faadac6b980bafe7861df52f8d88f55a8aca013
learningordeath <sdwfyfybh@163.com>
2019-05-30 16:59:08 +0800

    Config

--- class/Config.py
+++ class/Config.py
@@ -1,5 +1,5 @@
 class Config:
-    # 类属性
+
     mode = 0
     min_temp = 16
     max_temp = 30
@@ -11,16 +11,20 @@ class Config:
     FeeRate_M = 1
     FeeRate_L = 0.5
 
-    # 类方法
+    # 设置参数限制
     @staticmethod
-    def set_para(mode, min_temp, max_temp, default_temp, min_speed, max_speed, default_speed, rate_h, rate_m, rate_l):
-        Config.mode = mode
+    def set_para(min_temp, max_temp, min_speed, max_speed, rate_h, rate_m, rate_l):
         Config.min_temp = min_temp
         Config.max_temp = max_temp
-        Config.default_temp = default_temp
         Config.min_speed = min_speed
         Config.max_speed = max_speed
-        Config.default_speed = default_speed
         Config.FeeRate_H = rate_h
         Config.FeeRate_M = rate_m
         Config.FeeRate_L = rate_l
+
+    # 设置默认参数
+    @staticmethod
+    def set_default_para(mode, default_temp, default_speed):
+        Config.mode = mode
+        Config.default_temp = default_temp
+        Config.default_speed = default_speed
